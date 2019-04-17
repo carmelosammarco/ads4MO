a7ca3c6966b65176d6ddce0a9338136399d877bb
Carmelo <carmelosammarco@users.noreply.github.com>
2019-04-17 15:04:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setup(name='ads4mo',
-      version='0.0.4',
+      version='0.0.5',
       description='Python module which adds new CMEMS downloads services (applied mainly to big data requests)',
       long_description=long_description,
       long_description_content_type='text/markdown',
